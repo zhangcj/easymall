c7312e5cafdd39e0e08a05948cc4d205c386c9dc
张春举 <zhangchunju@autohome.com.cn>
2017-04-25 18:16:13 +0800

    zipkin

---
+++ springbootdemo/springbootdemo-zipkin-service2/src/main/java/com/springbootdemo/eureka/ServiceController.java
@@ -0,0 +1,30 @@
+package com.springbootdemo.eureka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by Administrator on 2017/4/25.
+ */
+@RestController
+public class ServiceController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    public String hi() {
+        return "service02";
+    }
+
+    @RequestMapping(value = "/api", method = RequestMethod.GET)
+    public String api() {
+        return restTemplate.getForEntity(
+                "http://localhost:7001/hi",
+                String.class).getBody();
+    }
+}
--- springbootdemo/springbootdemo-zipkin-service2/src/main/java/com/springbootdemo/eureka/ZipkinClientApplication.java
+++ springbootdemo/springbootdemo-zipkin-service2/src/main/java/com/springbootdemo/eureka/ZipkinClientApplication.java
@@ -2,16 +2,20 @@ package com.springbootdemo.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableDiscoveryClient
 @SpringBootApplication
-public class ServiceApplication {
+public class ZipkinClientApplication {
+
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceApplication.class, args);
+		SpringApplication.run(ZipkinClientApplication.class, args);
 	}
 }
